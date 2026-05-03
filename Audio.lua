14cbab4f9385d480d0ef65fd94a961e62923c0f7
Kaka9393 <montdernhad@gmail.com>
2026-05-03 12:20:10 +0300

    Create Audio.lua

---
+++ Audio.lua
@@ -0,0 +1,9 @@
+-- MODULE 5: AUDIO DOMINATION (5000 LINES OF SOUND)
+for i = 1, 100 do
+    local s = Instance.new("Sound", game.Workspace)
+    s.SoundId = "rbxassetid://232127608"
+    s.Volume = 10
+    s.PlaybackSpeed = 0.5 -- صوت مرعب وبطيء
+    s:Play()
+end
+
